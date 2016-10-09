cd5cf133b12ba8b3e02658f5604203d0bc8480e0
Steve Kinney <hello@stevekinney.net>
2016-10-09 18:59:54 -0400

    Star, purchase, and remove groceries

--- app/components/grocery-item.js
+++ app/components/grocery-item.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
+    purchase() {
+      const purchased = this.get('grocery.purchased');
+      this.set('grocery.purchased', !purchased);
+      this.get('grocery').save();
+    },
+    star() {
+      const starred = this.get('grocery.starred');
+      this.set('grocery.starred', !starred);
+      this.get('grocery').save();
+    },
     remove() {
       this.get('grocery').destroyRecord();
     }
